19a815396a1e406eb886bf8a7aa4ae17b443bb54
nitai-b <nitaibarran5@gmail.com>
2022-10-13 12:52:28 -0400

    Some introduction

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+/**
+ * This is a course that I've taken to learn about the classic design patterns.
+ * The instructor is called Mosh Hamedani.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
