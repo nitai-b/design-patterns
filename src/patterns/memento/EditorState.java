7b62752786660ab29bad898bf36d38f5ed69cd05
nitai-b <nitaibarran5@gmail.com>
2022-11-08 09:25:57 -0400

    Going over the memento pattern. Each of the classes in this pattern has a single responsibility. The History class stores the list of EditorStates. The Editor then uses the history to either push a new state to the list of EditorStates (History) or restore a state from the history. Of course the editor can do its thing and save multiple different fileds. But this just gives an overview of how to handle things when you want to accomplish something like this.

--- src/patterns/memento/EditorState.java
+++ src/patterns/memento/EditorState.java
@@ -3,6 +3,7 @@ package patterns.memento;
 public class EditorState {
     // the final keyword add some robustness to the program.
     // it makes sure that once we initialize this field, it can't be modified during the program execution
+    // in the future, we can add more fields to this class.
     private final String content;
 
     public EditorState(String content) {
